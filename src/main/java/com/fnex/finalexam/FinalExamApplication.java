137de8c774992292de7efeead460c80d01e9d84e
NHVHung19 <130159103+NHVHung19@users.noreply.github.com>
2024-03-17 20:48:20 +0700

    update

---
+++ src/main/java/com/fnex/finalexam/FinalExamApplication.java
@@ -0,0 +1,13 @@
+package com.fnex.finalexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalExamApplication.class, args);
+    }
+
+}
